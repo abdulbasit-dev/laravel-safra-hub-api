3ddf58841f4ccbf7923b06437f46327cc4b93e77
abdulbasit-web <abdullbasit99salah@gmail.com>
2021-07-27 12:58:15 +0300

    add test users api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,10 +19,12 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
 Route::get('test', function () {
-    if (auth()->check()) {
-        return 'sd';
-    }
     return 'test';
+});
+
+
+Route::get('users', function () {
+    return User::all();
 });
 
 
