8bdb1a50306519b4225c0e65e2684dbeecea34ab
abdulbasit-web <abdullbasit99salah@gmail.com>
2021-08-14 23:34:11 +0300

    add localization middleware

--- routes/api.php
+++ routes/api.php
@@ -21,35 +21,39 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-//PROTECTED ROUTES
-Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('email/verification-notification', [EmailVerificationController::class, 'sendVerificationEmail']);
-    Route::post('email/check-verification', [EmailVerificationController::class, 'checkVerification']);
-    Route::group(['namespace' => 'Api'], function () {
-        Route::resource('user-favorate-items', \App\Http\Controllers\Api\UserFavItemController::class)->except('create', 'edit', 'update');
-        Route::get('/user-profiles', [UserProfileController::class, 'index']);
-        Route::get('/user-profiles/{user}', [UserProfileController::class, 'userProfileById']);
-        Route::put('/user-profiles', [UserProfileController::class, 'update']);
-        Route::get('/user-profiles/friends', [UserProfileController::class, 'userFriends']);
+
+Route::group(['middleware' => 'localization'], function () {
+    //PROTECTED ROUTES
+    Route::group(['middleware' => ['auth:sanctum']], function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::post('email/verification-notification', [EmailVerificationController::class, 'sendVerificationEmail']);
+        Route::post('email/check-verification', [EmailVerificationController::class, 'checkVerification']);
+        Route::group(['namespace' => 'Api'], function () {
+            Route::resource('user-favorate-items', \App\Http\Controllers\Api\UserFavItemController::class)->except('create', 'edit', 'update');
+            Route::get('/user-profiles', [UserProfileController::class, 'index']);
+            Route::get('/user-profiles/{user}', [UserProfileController::class, 'userProfileById']);
+            Route::put('/user-profiles', [UserProfileController::class, 'update']);
+            Route::get('/user-profiles/friends', [UserProfileController::class, 'userFriends']);
+        });
     });
-});
 
 
-//PUBLIC ROUTES
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('verify-email/{id}/{hash}', [EmailVerificationController::class, 'verify'])->name('verification.verify');
-Route::get('/all-user', function () {
-    return User::orderByDesc('id')->get();
-});
-Route::get('test', function () {
-    //generate uniq code
-    // return str::upper(Str::random(6));
-    return 'test';
-});
+    //PUBLIC ROUTES
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('verify-email/{id}/{hash}', [EmailVerificationController::class, 'verify'])->name('verification.verify');
+    Route::get('/all-user', function () {
+        return User::orderByDesc('id')->get();
+    });
+    Route::get('test', function () {
+        //generate uniq code
+        // return str::upper(Str::random(6));
+        return 'test';
+    });
 
-Route::group(['namespace' => 'Api'], function () {
-    Route::resource('picnics', PicnicController::class)->except('create', 'edit');
-    Route::resource('categories', CategoryController::class)->except('create', 'edit');
+    Route::group(['namespace' => 'Api'], function () {
+        Route::resource('picnics', PicnicController::class)->except('create', 'edit');
+        Route::resource('categories', CategoryController::class)->except('create', 'edit');
+    });
+
 });
