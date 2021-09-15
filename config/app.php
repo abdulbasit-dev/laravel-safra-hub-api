bc9d12f6e1cbaea5e375be644d3e67d51bcd4fda
abdulbasit-web <abdullbasit99salah@gmail.com>
2021-09-15 14:34:20 +0300

    fix

--- config/app.php
+++ config/app.php
@@ -166,7 +166,7 @@ return [
          * Package Service Providers...
          */
         Intervention\Image\ImageServiceProvider::class,
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+//        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         /*
          * Application Service Providers...
          */
